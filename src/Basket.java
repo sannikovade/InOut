bd70a5a20c62e42335105ef0108c9b013903b316
Daria Sannikova <sannikovade@gmail.com>
2023-02-24 21:05:40 +0300

    Initial commit

---
+++ src/Basket.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Basket {
+    private static String[] products;
+    private static int[] prices;
+
+    private static int[] amountOfProducts;
+    private static int[] costOfProducts;
+    private static boolean isFilled[];
+    private static int[] in;
+
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        amountOfProducts = new int[products.length];
+        costOfProducts = new int[products.length];
+        isFilled = new boolean[products.length];
+    }
+
+
+
+    public void addToCart(int productNum, int amount){
+        amountOfProducts[productNum] += amount;
+        costOfProducts[productNum] += (amount * prices[productNum]);
+        isFilled[productNum] = true;
+    }
+    public void printCart(){
+        System.out.println("Your cart:");
+        for (int i = 0; i < products.length; i++) {
+            if (isFilled[i]) {
+                System.out.println(products[i] + " x " + amountOfProducts[i] + "pc. - " + costOfProducts[i] + " RUB");
+            }
+        }
+        int total = 0;
+        for (int prodCost:
+             costOfProducts) {
+            total += prodCost;
+        }
+        System.out.println("Total: " + total + " RUB");
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (FileWriter writer = new FileWriter(textFile)) {
+            for (int i : amountOfProducts) {
+                writer.write(i + " ");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    public static Basket loadFromTextFile(File file) {
+        Basket basket = new Basket(products, prices);
+        try (FileReader reader = new FileReader("cart.txt")){
+            Scanner scanner = new Scanner(reader);
+            while (scanner.hasNextLine()){
+                String[] input = scanner.nextLine().split(" ");
+                in = Arrays.stream(input)
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        for (int i = 0; i < in.length; i++) {
+            basket.addToCart(i,in[i]);
+        }
+
+        return basket;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String[] products = {"Milk", "Butter", "Oatmeal"};
+        int[] prices = {60, 100, 50};
+        System.out.println("Products available for purchase:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + " " + products[i] + " " + prices[i] + " RUB/1pc");
+        }
+
+        Basket basket = new Basket(products,prices);
+        File file = new File("./cart.txt");
+
+        if(file.exists()){
+            basket = Basket.loadFromTextFile(file);
+        }
+
+        while (true) {
+
+            System.out.println("Select item and quantity or enter 'end'");
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            String[] parts = input.split(" ");
+            int productNumber = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+            basket.addToCart(productNumber, productCount);
+            basket.saveTxt(file);
+
+        }
+        basket.printCart();
+    }
+}
