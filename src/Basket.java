07b640e4c89f83009e6173bd2f2d892be825b96e
Daria Sannikova <sannikovade@gmail.com>
2023-02-25 14:25:34 +0300

    task 2 static fixed

--- src/Basket.java
+++ src/Basket.java
@@ -1,16 +1,12 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Basket implements Serializable {
     private static final long serialVersionUID = 1L;
-    private static String[] products;
-    private static int[] prices;
-
-    private static int[] amountOfProducts;
-    private static int[] costOfProducts;
-    private static boolean isFilled[];
-    private static int[] in;
+    private String[] products;
+    private int[] prices;
+    private int[] amountOfProducts;
+    private int[] costOfProducts;
+    private boolean isFilled[];
 
     public Basket(String[] products, int[] prices) {
         this.products = products;
@@ -20,14 +16,24 @@ public class Basket implements Serializable {
         isFilled = new boolean[products.length];
     }
 
+    static Basket loadFromBinFile(File file) {
+        Basket basket = null;
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            basket = (Basket) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return basket;
+    }
 
-
-    public void addToCart(int productNum, int amount){
+    public void addToCart(int productNum, int amount) {
         amountOfProducts[productNum] += amount;
         costOfProducts[productNum] += (amount * prices[productNum]);
         isFilled[productNum] = true;
     }
-    public void printCart(){
+
+    public void printCart() {
         System.out.println("Your cart:");
         for (int i = 0; i < products.length; i++) {
             if (isFilled[i]) {
@@ -35,61 +41,20 @@ public class Basket implements Serializable {
             }
         }
         int total = 0;
-        for (int prodCost:
-             costOfProducts) {
+        for (int prodCost :
+                costOfProducts) {
             total += prodCost;
         }
         System.out.println("Total: " + total + " RUB");
     }
 
-    public void saveTxt(File textFile) throws IOException {
-        try (FileWriter writer = new FileWriter(textFile)) {
-            for (int i : amountOfProducts) {
-                writer.write(i + " ");
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-     static Basket loadFromTextFile(File file) {
-        Basket basket = new Basket(products, prices);
-        try (FileReader reader = new FileReader("cart.txt")){
-            Scanner scanner = new Scanner(reader);
-            while (scanner.hasNextLine()){
-                String[] input = scanner.nextLine().split(" ");
-                in = Arrays.stream(input)
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
-            }
-
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        for (int i = 0; i < in.length; i++) {
-            basket.addToCart(i,in[i]);
-        }
-
-        return basket;
-    }
-
     public void saveBin(File file, Basket basket) {
         try (FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos)){
-                oos.writeObject(basket);
-            }  catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    static Basket loadFromBinFile(File file) {
-        Basket basket = null;
-        try (FileInputStream fis = new FileInputStream(file);
-        ObjectInputStream ois = new ObjectInputStream(fis)){
-            basket = (Basket) ois.readObject();
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(basket);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-        return basket;
     }
 
 }
